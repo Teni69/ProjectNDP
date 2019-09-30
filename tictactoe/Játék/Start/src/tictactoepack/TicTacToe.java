eb5477f857cb060e1b6ca1a84b073c783caaa6b2
Teni <d3zsooo@gmail.com>
2019-09-30 18:22:28 +0200

    Startup felület, játék

---
+++ tictactoe/Játék/Start/src/tictactoepack/TicTacToe.java
@@ -0,0 +1,16 @@
+package tictactoepack;
+
+import javax.swing.JFrame;
+
+public class TicTacToe {
+	
+	public static void jatekkezdes() {
+		JFrame ticTacToe = new TicTacToeFrame();
+		ticTacToe.setTitle("TicTacToe Game");
+		ticTacToe.setSize(400, 400);
+		ticTacToe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ticTacToe.setResizable(false);
+		ticTacToe.setLocationRelativeTo(null);
+		ticTacToe.setVisible(true);
+	}
+}
